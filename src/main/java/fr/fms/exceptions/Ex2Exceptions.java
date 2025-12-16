9d790e338885b6b9e712d5877d639987a100ef9f
Marie-Lorraine Collignon <marie-lorraine.collignon@fms-ea.com>
2025-12-16 15:22:36 +0100

    Added EX 2.1

---
+++ src/main/java/fr/fms/exceptions/Ex2Exceptions.java
@@ -0,0 +1,32 @@
+package fr.fms.exceptions;
+
+import java.util.Date;
+
+import fr.fms.utils.AppLogger;
+
+public class Ex2Exceptions {
+    public static void main(String[] args) {
+        Date date = null;
+        Date today = new Date();
+
+        try {
+            // Throw NullPointerException because date is null
+            System.out.println("getClass() result : " + date.getClass().getName());
+        } catch (NullPointerException e) {
+            // Handle error instead of crash
+            AppLogger.error("!!! Date is null -> " + e.getClass().getSimpleName());
+        } finally {
+            // Always executed
+            System.out.println("Finally executed");
+        }
+        try {
+            // Works because today is !null
+            System.out.println("getClass() result : " + today.getClass().getName());
+        } catch (NullPointerException e) {
+            AppLogger.error("⚠️ Date is null -> " + e.getClass().getSimpleName());
+        } finally {
+            // Always executed
+            System.out.println("Finally executed");
+        }
+    }
+}
